43ebedbfaf2a632c56c37cf80eb177b1ddf5cbe6
Carlos Alberto Velez Delgado <58532485+Carlosvelez1999@users.noreply.github.com>
2025-05-25 20:41:06 -0500

    feat: incluir entorno APP_ENV en registro de Google Sheets

--- sheets_utils.py
+++ sheets_utils.py
@@ -1,32 +1,28 @@
 import os
+import json
 import gspread
 import pytz
-from google.oauth2.service_account import Credentials
-from datetime import datetime, timedelta  
-import traceback
-import json
-
-import os
-import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
-import pytz
-import json
 
 # Leer JSON desde variable de entorno
 json_str = os.getenv("GOOGLE_CREDENTIALS_JSON")
 info = json.loads(json_str)
 
+# Leer entorno (dev o prod)
+APP_ENV = os.getenv("APP_ENV", "dev")
+
+# Alcances
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Inicializar cliente
+# Inicializar cliente de Google Sheets
 credentials = Credentials.from_service_account_info(info, scopes=SCOPES)
 client = gspread.authorize(credentials)
 
-# Configuración de hoja
+# ID de la hoja (puede ser una sola hoja para ambos entornos)
 SHEET_ID = "1rWdlpUYMYgJRrCL9WCjzNL-0wS0pzI3zCcH3OJFnRVw"
 SHEET_NAME = "Hoja 1"
 
@@ -35,7 +31,7 @@ def registrar_prediccion(nombre_imagen, etiqueta, confianza):
         hoja = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
         zona_colombia = pytz.timezone("America/Bogota")
         timestamp = datetime.now(zona_colombia).strftime("%Y-%m-%d %H:%M:%S")
-        fila = [timestamp, nombre_imagen, etiqueta, f"{confianza * 100:.2f}%"]
+        fila = [timestamp, nombre_imagen, etiqueta, f"{confianza * 100:.2f}%", APP_ENV]
         hoja.append_row(fila)
         print("✅ Predicción registrada en Google Sheets")
     except Exception as e:
