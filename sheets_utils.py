c295b4491fb01c2e565104e7bfcd8e3024ce3c18
Carlos Alberto Velez Delgado <58532485+Carlosvelez1999@users.noreply.github.com>
2025-05-25 19:33:15 -0500

    Agregar integración con Google Sheets y hora local

--- sheets_utils.py
+++ sheets_utils.py
@@ -4,37 +4,40 @@ import pytz
 from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta  
 import traceback
+import json
 
-# Ruta del archivo de credenciales
-CREDENTIALS_PATH = "credentials/credentials.json"
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+import pytz
+import json
 
-# Alcances requeridos para usar Google Sheets
+# Leer JSON desde variable de entorno
+json_str = os.getenv("GOOGLE_CREDENTIALS_JSON")
+info = json.loads(json_str)
+
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Inicializar cliente de Google Sheets
-credentials = Credentials.from_service_account_file(CREDENTIALS_PATH, scopes=SCOPES)
+# Inicializar cliente
+credentials = Credentials.from_service_account_info(info, scopes=SCOPES)
 client = gspread.authorize(credentials)
 
-# ID de la hoja de cálculo (reemplaza por la tuya)
+# Configuración de hoja
 SHEET_ID = "1rWdlpUYMYgJRrCL9WCjzNL-0wS0pzI3zCcH3OJFnRVw"
-
-# Nombre de la hoja dentro del archivo (ej. "Hoja 1")
 SHEET_NAME = "Hoja 1"
 
 def registrar_prediccion(nombre_imagen, etiqueta, confianza):
     try:
         hoja = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
-
-        # Hora ajustada a Colombia (UTC−5)
         zona_colombia = pytz.timezone("America/Bogota")
         timestamp = datetime.now(zona_colombia).strftime("%Y-%m-%d %H:%M:%S")
-
         fila = [timestamp, nombre_imagen, etiqueta, f"{confianza * 100:.2f}%"]
         hoja.append_row(fila)
         print("✅ Predicción registrada en Google Sheets")
     except Exception as e:
         print("⚠️ Error al registrar en Sheets:")
-        traceback.print_exc()
+        print(e)
